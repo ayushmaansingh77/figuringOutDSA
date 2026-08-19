4ad013b6010507dde5b644ad9d810548bd5e4066
Ayushmaan singh <ayushmaansingh742@gmail.com>
2026-08-20 00:24:34 +0530

    Add 101_symmetric_tree.cpp - LeetCode Problem 101

---
+++ 101_symmetric_tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+bool issame(TreeNode* a,TreeNode* b)
+{
+    if(!a && !b)
+    {return true;}
+    if(!a||!b)
+    {return false;}
+    return a->val==b->val && issame(a->right,b->left)&&issame(a->left,b->right);
+}
+    bool isSymmetric(TreeNode* root) {
+       return issame(root->right,root->left);
+    }
+};
